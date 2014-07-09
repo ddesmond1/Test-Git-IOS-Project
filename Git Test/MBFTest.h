a852b9676ff843569ed3fa8b648c96b8456f5bff
Donald Desmond <Donald@Donalds-MacBook-Pro.local>
2014-07-09 16:11:38 -0400

    added a test class

---
+++ Git Test/MBFTest.h
@@ -0,0 +1,13 @@
+//
+//  MBFTest.h
+//  Git Test
+//
+//  Created by Donald Desmond on 2014-07-09.
+//  Copyright (c) 2014 Play Me Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFTest : NSObject
+
+@end
---
+++ Git Test/MBFTest.m
@@ -0,0 +1,13 @@
+//
+//  MBFTest.m
+//  Git Test
+//
+//  Created by Donald Desmond on 2014-07-09.
+//  Copyright (c) 2014 Play Me Interactive. All rights reserved.
+//
+
+#import "MBFTest.h"
+
+@implementation MBFTest
+
+@end
